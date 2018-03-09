996cc19228705273a69fb4e8e78bc959d93294b2
dzhu <dzhu@amxc.tech>
2018-03-09 16:33:33 +0800

    Initial commit

---
+++ app/src/main/java/com/zhudong/easystepview/StepItemData.java
@@ -0,0 +1,23 @@
+package com.zhudong.easystepview;
+
+public class StepItemData {
+
+    private String msg;
+    private String date;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
